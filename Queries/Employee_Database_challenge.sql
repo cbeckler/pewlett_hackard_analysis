a51bbd0c62cbe208e738b1ca4863bc1d638f466a
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2022-09-14 09:36:26 -0500

    create mentorship_eligibility table

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- create retirement_titles table
 SELECT
 	e.emp_no,
 	e.first_name,
@@ -38,6 +39,7 @@ ORDER BY
 	
 SELECT * FROM unique_titles LIMIT 10;
 
+-- get N employees retiring by title
 SELECT 
 	COUNT(*),
 	title
@@ -51,3 +53,33 @@ ORDER BY
 	COUNT(*) DESC;
 	
 SELECT * FROM retiring_tables;
+
+-- Create mentorship_eligibility table
+SELECT 
+	DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date,
+	tt.title
+INTO 
+	mentorship_eligibility
+FROM 
+	employees e
+INNER JOIN 
+	dept_emp d 
+ON 
+	e.emp_no = d.emp_no
+INNER JOIN 
+	titles tt 
+ON 
+	e.emp_no = tt.emp_no
+WHERE 
+	(d.to_date = '9999-01-01') 
+AND 
+	(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY 
+	e.emp_no;
+	
+SELECT * FROM mentorship_eligibility LIMIT 10;
